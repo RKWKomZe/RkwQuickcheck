f6cab54f054d72b139c8b5aecd7b239d921f7994
Steffen Kroggel <kroggel@rkw.de>
2023-03-24 19:41:58 +0100

    [FIX] minor fixes and improvements

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,6 +25,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
+            'core_extended' => '9.5.4-9.5.99',
             'rte_ckeditor' => '9.5.0-9.5.99',
         ],
         'conflicts' => [],
